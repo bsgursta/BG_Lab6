e61548676d26a433eca03ddb76a950dde81236a4
Donald Woelke <donaldaw313@gmail.com>
2023-10-31 12:02:05 -0400

    Donald Woelke

--- version_control.py
+++ version_control.py
@@ -11,9 +11,9 @@ def encoder(in_str):
         en_str += str(indiv_char)
     return en_str
 
-def decode_password(encoded_password):
+def decode_password(en_str):
     decoded_password = ""
-    for digit in encoded_password:
+    for digit in en_str:
         decoded_digit = str((int(digit) - 3) % 10)
         decoded_password += decoded_digit
     return decoded_password
@@ -31,6 +31,11 @@ def main():
             user_str = input("Please enter your password to encode: ")
             encoded_pass = encoder(user_str)
             print("Your password has been encoded and stored!")
+        elif user_inp == "2":
+            if 'encoded_pass' in locals():
+                print(f"The encoded password is {encoded_pass}, and the original password is {decode_password(encoded_pass)}.")
+            else:
+                print("No encoded password has been stored yet.")
         elif user_inp == '3':
             n = 0
 if __name__=="__main__":
