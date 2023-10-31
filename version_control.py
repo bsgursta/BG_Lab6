fd82038f3309485d1b9d0cae7e34e7f727e1128f
Donald Woelke <donaldaw313@gmail.com>
2023-10-31 11:36:39 -0400

    Donald Woelke

--- version_control.py
+++ version_control.py
@@ -11,6 +11,13 @@ def encoder(in_str):
         en_str += str(indiv_char)
     return en_str
 
+def decode_password(encoded_password):
+    decoded_password = ""
+    for digit in encoded_password:
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_password += decoded_digit
+    return decoded_password
+
 def main():
     n = 1
     while n == 1:
@@ -19,7 +26,7 @@ def main():
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
-        user_inp = input("Please enter and option: ")
+        user_inp = input("Please enter an option: ")
         if user_inp == "1":
             user_str = input("Please enter your password to encode: ")
             encoded_pass = encoder(user_str)
